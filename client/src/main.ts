66e3acd7e5538d182f336364f43b7556c2b7c47c
tropicbliss <tropicbliss@protonmail.com>
2024-11-05 15:04:48 +0800

    E

--- client/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "@/assets/index.css";
-import { VueQueryPlugin } from "@tanstack/vue-query";
-
-const app = createApp(App);
-app.use(VueQueryPlugin);
-app.mount("#app");
